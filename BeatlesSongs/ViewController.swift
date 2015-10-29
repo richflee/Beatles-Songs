ea7ee5c27021218a29c49212fc61ae9cefb879be
Richard Lee <rich@richards-macbook-pro.local>
2015-10-29 19:27:22 +1100

    Initial Commit

---
+++ BeatlesSongs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeatlesSongs
+//
+//  Created by Richard Lee on 29/10/2015.
+//  Copyright © 2015 Richard Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
